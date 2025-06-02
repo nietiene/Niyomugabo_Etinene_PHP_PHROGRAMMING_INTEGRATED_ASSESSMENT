02f478fee025e077ca317d70e5c65682be87f2f3
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 15:00:39 -0700

    commit of gikonko-tss-managment-system

--- addModule.php
+++ addModule.php
@@ -1,46 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Module</title>
-</head>
-<body>
-    <form action="" method="post">
-        <label for="">Module Name</label>
-        <input type="text" name="Module_Name" /> <br>
-        <select name="Trade_Id" >
-            <?php
-               include("conn.php");
-               $sql = "SELECT * FROM trades";
-               $query = mysqli_query($conn, $sql);
-               while ($data = mysqli_fetch_assoc($query)) {
-                echo "<option value='" . $data['Trade_id'] . "'>" . $data['Trade_name'] . "</option>";
-               }
-            ?>
-        </select>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Module</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <label for="">Module Name</label>
+            <input type="text" name="Module_Name" /> <br>
+            <select name="Trade_Id" >
+                <?php
+                include("conn.php");
+                $sql = "SELECT * FROM trades";
+                $query = mysqli_query($conn, $sql);
+                while ($data = mysqli_fetch_assoc($query)) {
+                    echo "<option value='" . $data['Trade_id'] . "'>" . $data['Trade_name'] . "</option>";
+                }
+                ?>
+            </select>
 
-        <button name="addModule">Save Module</button>
-    </form>
+            <button name="addModule">Save Module</button>
+        </form>
 
-    <?php
-       include("conn.php");
-       if (isset($_POST['addModule'])) {
-        $Module_Name = $_POST['Module_Name'];
-        $Trade_Id = $_POST['Trade_Id'];
-        $sql = "INSERT INTO modules(Module_Name, Trade_Id) VALUES('$Module_Name, '$Trade_Id')";
-        $query = mysqli_query($conn, $sql);
+        <?php
+        
+        if (isset($_POST['addModule'])) {
+                $Module_Name = $_POST['Module_Name'];
+                $Trade_Id = $_POST['Trade_Id'];
+                
+                $sql = "INSERT INTO modules(Module_Name, Trade_Id) VALUES('$Module_Name', '$Trade_Id')";
+                $query = mysqli_query($conn, $sql);
 
-        if ($query) {
-            echo "Module Saved Successfully";
-        } else {
-            die (mysqli_error($conn));
+                if ($query) {
+                echo "Module Saved Successfully";
+                } else {
+                    die (mysqli_error($conn));
+                }
         }
-       }
-    
-    
-    
-    
-    ?>
-</body>
-</html>
+        
+        
+        
+        
+        ?>
+    </body>
+    </html>
