c2de0b59d14c5209f3c760968190c4f64a935c25
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 13:50:03 -0700

    commit of gikonko-tss-managment-system

--- insert.php
+++ insert.php
@@ -1,6 +1,25 @@
-<?php
-  include('conn.php');
-  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Trainee</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Firstname</label>
+        <input type="text" name="Firstname" > <br>
 
+        <label for="">Last Name</label>
+        <input type="text" name="latname" > <br>
 
-?>
+        <label for="">Gender</label>
+        <select name="gender" id="">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select> <br>
+        <label >Trade Id</label>
+        <input type="text" name="Trade_id" /> <br>
+    </form>
+</body>
+</html>
