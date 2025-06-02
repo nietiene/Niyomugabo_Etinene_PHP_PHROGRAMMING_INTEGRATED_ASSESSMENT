f7c657038230dc6ca4978f64c5b9f7e1aa63093b
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 15:03:50 -0700

    commit of gikonko-tss-managment-system

---
+++ addMarks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Trainee Code</label>
+        <input type="text" name="Trade_Id" id=""> <br>
+        <label for="">Module Name</label>
+        <select name="moduleId" id="">
+            <?php
+               include('conn.php');
+               
+            ?>
+        </select>
+    </form>
+</body>
+</html>
