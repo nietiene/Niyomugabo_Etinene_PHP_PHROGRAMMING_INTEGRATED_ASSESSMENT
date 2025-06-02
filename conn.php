828b62cf7601513709dc8c4cda634f01777491c7
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 13:43:07 -0700

    first commit of gikonko-tss-managment-system

--- conn.php
+++ conn.php
@@ -1,3 +1,10 @@
 <?php
 
+$conn = mysqli_connect("localhost", "root", "factorise@123", "gikonko_tss_managment_system");
+
+if ($conn) {
+    echo "Connected successfuly";
+} else {
+    die("Error" . mysqli_connect_error());
+}
 ?>
