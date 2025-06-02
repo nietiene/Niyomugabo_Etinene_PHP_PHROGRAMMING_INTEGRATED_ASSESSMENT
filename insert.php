ec74a877cb4da3a7f1c977f5da76792328f65652
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 14:31:31 -0700

    commit of gikonko-tss-managment-system

--- insert.php
+++ insert.php
@@ -1,44 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add New Trainee</title>
-</head>
-<body>
-    <form action="" method="post">
-        <label for="">Firstname</label>
-        <input type="text" name="Firstname" > <br>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add New Trainee</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <label for="">Firstname</label>
+            <input type="text" name="Firstname" > <br>
 
-        <label for="">Last Name</label>
-        <input type="text" name="latname" > <br>
+            <label for="">Last Name</label>
+            <input type="text" name="latname" > <br>
 
-        <label for="">Gender</label>
-        <select name="gender" id="">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select> <br>
-        <label >Trade Id</label>
-        <input type="text" name="Trade_id" /> <br>
+            <label for="">Gender</label>
+            <select name="gender" >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select> <br>
+            <label >Trade Id</label>
+              <select name="trade_id">
+                <?php
+                  include("conn.php")
 
-        <button name="add">Save</button>
-    </form>
+                ?>
+              </select><br>
 
-    <?php
-        include("conn.php");
-       
-        if (isset($_POST['add'])) {
-            $firstName = $_POST['Firstname'];
-            $lastName = $_POST['latname'];
-            $gender = $_POST['gender'];
-            $trade_id = $_POST['Trade_id'];
+            <button name="add">Save</button>
+        </form>
 
-            $sql = "INSERT INTO trainees(firstName,latname,gender,Trade_id) VALUES('$firstName', '$lastName', $gender, $trade_id)";
-            
-        }
+        <?php
+            include("conn.php");
+        
+            if (isset($_POST['add'])) {
+                $firstName = $_POST['Firstname'];
+                $lastName = $_POST['latname'];
+                $gender = $_POST['gender'];
+                $trade_id = $_POST['Trade_id'];
+
+                $sql = "INSERT INTO trainees(firstName,latname,gender,Trade_id) VALUES('$firstName', '$lastName', '$gender', '$trade_id')";
+                $result = mysqli_query($conn, $sql);
+                if ($result) {
+                    echo "Data inserted Successfully";
+                } else {
+                    echo "Error" . mysqli_error($conn);
+                } 
+            }
 
 
 
-    ?>
-</body>
-</html>
+        ?>
+    </body>
+    </html>
