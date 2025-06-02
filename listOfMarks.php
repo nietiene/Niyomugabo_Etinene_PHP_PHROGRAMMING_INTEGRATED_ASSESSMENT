f84b9b820e1f93741bf424d73d10cf943605824e
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 16:21:43 -0700

    commit of gikonko-tss-managment-system

--- listOfMarks.php
+++ listOfMarks.php
@@ -6,6 +6,32 @@
     <title>List Of Marks</title>
 </head>
 <body>
-    
+    <table border="2" cellpadding="5" cellspacing="2">
+       <tr>
+          <th>Mark Code</th>
+          <th>Trainee Code</th>
+          <th>Module Code</th>
+          <th>Formative Assessment /50</th>
+          <th>Formative Assessment /50</th>
+          <th>Total /100</th>
+          <th>Decision</th>
+        </tr>
+       
+     <?php
+         include ("conn.php");
+          $sql = "SELECT * FROM marks";
+          $query = mysqli_query($sql);
+
+          while ($data = mysqli_fetch_assoc($query)) {
+            echo 
+               "
+               <tr>
+                   <td>{}</td>
+               
+               "
+          }
+
+     ?>
+    </table>
 </body>
 </html>
