dd3826b57fb78cafdccd42accb9a2987367f85f8
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 17:45:12 -0700

    commit of gikonko-tss-managment-system

---
+++ updateTrainee.php
@@ -0,0 +1,18 @@
+<?php
+     include ('conn.php');
+    
+     if (isset($_GET['Trainee_Id'])) {
+         $Trainee_Id = $_GET['Trainee_Id'];
+         $sql = "SELECT t.Trainee_Name,
+                 t.Firstname t.lastname,
+                 t.Gender,
+                  t.Trade_Id,
+                  td.Trade_Name
+                  FROM trainees t 
+                  JOIN trades td
+                  ON t.Trade_Id = td."
+     }
+
+
+
+?>
