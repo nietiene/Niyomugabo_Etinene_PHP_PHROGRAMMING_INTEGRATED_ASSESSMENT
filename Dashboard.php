da38c26946785c988f20d79ae9f4c06bb8b5ad22
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 17:04:50 -0700

    commit of gikonko-tss-managment-system

--- Dashboard.php
+++ Dashboard.php
@@ -1,11 +1,15 @@
+<?php
+   session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DOS Dashboard</title>
 </head>
 <body>
-    <h2>Welcome <?php echo $_SESSION['Usename'];?></h2>
+    <h2>Welcome <?php echo $_SESSION['Usename'];?></h2> <br>
+    <a href="listOfTrainee.php">Manage User</a>
 </body>
 </html>
