c39d4b40bfbc7ee709abc0514fef9ca26b1c716c
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 20:04:26 -0700

    commit of gikonko-tss-managment-system

---
+++ deleteTrade.php
@@ -0,0 +1,14 @@
+<?php
+     include("conn.php");
+     
+     $Trade_Id = $_GET['Trade_id'];
+     $sql = "DELETE FROM trades WHERE Trade_id = '$Trade_Id'";
+     $result = mysqli_query($conn, $sql);
+
+     if ($result) {
+        header("Location:listOfTrades.php");
+     } else {
+        die ("ERROR". mysqli_error($conn));
+     }
+
+?>
