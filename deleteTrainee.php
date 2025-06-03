ada3ff15d8d551d64ad38b711bd46d50e10e2ea3
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 18:56:17 -0700

    commit of gikonko-tss-managment-system

--- deleteTrainee.php
+++ deleteTrainee.php
@@ -2,10 +2,12 @@
    include ("conn.php");
    
    $Trainee_Id = $_GET['Trainee_Id'];
+   
+   $delete_In_Marks_Table = mysqli_query($conn, "DELETE FROM marks WHERE Trainee_Id = '$Trainee_Id'");
    $sql = "DELETE FROM trainees WHERE Trainee_Id = '$Trainee_Id'";
    $result = mysqli_query($conn, $sql);
    
-   if ($result) {
+   if ($result && $delete_In_Marks_Table) {
     echo "Trainee Deleted";
     header("Location:listOfTrainee.php");   
 } else {
