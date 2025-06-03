461632ffe946f03e55be80ded3c9cafcf3104bbb
nietiene <niyomugaboetiene53@gmail.com>
2025-06-03 12:55:11 -0700

    commit of gikonko-tss-managment-system

--- listOfModule.php
+++ listOfModule.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Of Modules</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <a href="addModule.php">Add New Module</a>
+<body class="min-h-screen bg-green-100 flex justify-center items-center">
+    <div class="bg-blue-300 p-8 px-10 py-10 shadow-2xl rounded-md">
+    <div class="flex justify-end">
+       <a href="addModule.php" class="mb-7 bg-green-500 py-2 px-5 text-white rounded-lg hover:bg-green-700">Add New Module</a>
+    </div>
     <table border="2" cellspacing="2" cellpadding="5">
         <tr>
           <th>Module Code</th>
@@ -49,5 +53,6 @@
             }
        ?>
     </table>
+    </div>
 </body>
 </html>
