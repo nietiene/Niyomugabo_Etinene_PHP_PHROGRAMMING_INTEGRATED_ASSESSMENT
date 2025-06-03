a3ce2d7690a5eb07efb1a585659a1af24cff5c44
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 19:55:02 -0700

    commit of gikonko-tss-managment-system

--- updateTrade.php
+++ updateTrade.php
@@ -1,17 +1,35 @@
 <?php
  
      include ("conn.php");
-     if (isset($_GET['Trade_Id'])) {
-          $Trade_Id = $_GET['Trade_Id'];
-          $sql = "SELECT * FROM trades WHERE Trade_Id = $Trade_Id";
-          $result = mysqli_query($sql);
+     if (isset($_GET['Trade_id'])) {
+          $Trade_Id = $_GET['Trade_id'];
+          $sql = "SELECT * FROM trades WHERE Trade_id = $Trade_Id";
+          $result = mysqli_query($conn, $sql);
           
-          if (mysqli_num_rows($result)) {
+          if (mysqli_num_rows($result) > 0) {
             $Data = mysqli_fetch_assoc($result);
           } else {
             die("ERROR:" . mysqli_error($conn));
           }
-        
     }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Trade</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Trade Code</label>
+        <input type="text" name="Trade_id" value=<?php echo $Data['Trade_id']?>> <br>
+
+        <label for="">Trade Name</label>
+        <input type="text" name="Trade_Name" value=<?php echo $Data['Trade_name']?>> <br>
+      
+        <button name="save">Save Changes</button>
+    </form>
+</body>
+</html>
