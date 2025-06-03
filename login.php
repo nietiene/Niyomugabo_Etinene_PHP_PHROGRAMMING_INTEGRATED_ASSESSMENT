3a8dad6f6ac603d8dc6a1b3e3f90aed259375393
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 17:02:57 -0700

    commit of gikonko-tss-managment-system

--- login.php
+++ login.php
@@ -16,15 +16,19 @@
     </form>
 
     <?php
-        include ('conn.php');
+       include ('conn.php');
+       session_start();
        if (isset($_POST['login'])) {
             $username = $_POST['Usename'];
             $password = $_POST['Password'];
 
-            $sql = "SELECT * FROM users WHERE Username='$username' AND Password='$password'";
-            $result = mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($result) > 0) {
+            $sql = "SELECT * FROM users WHERE Usename='$username' AND Password='$password'";
+            $data = mysqli_query($conn, $sql);
+            
+            if (!$data) {
+                echo "ERROR". mysqli_error($conn);
+            }
+            if (mysqli_num_rows($data) > 0) {
                 $_SESSION['Usename'] = $username;
                 header('Location:dashboard.php');
             } else {
