fff07f416fa13932e81486d3e730a4cd5e5b7ac4
nietiene <niyomugaboetiene53@gmail.com>
2025-06-03 09:05:31 -0700

    commit of gikonko-tss-managment-system

--- listOfModule.php
+++ listOfModule.php
@@ -6,7 +6,18 @@
            t.Trade_Name
            FROM modules m 
            JOIN trades d
-           ON "
-
-
+           ON t.Trade_Id = m.Trade_Id";
+   $result = mysqli_query($conn, $sql);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Of Modules</title>
+</head>
+<body>
+    
+</body>
+</html>
