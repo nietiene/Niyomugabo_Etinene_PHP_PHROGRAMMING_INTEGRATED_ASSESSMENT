8e75d89ce02f05f4e6fa9a9966f36f06bdded010
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 17:09:24 -0700

    commit of gikonko-tss-managment-system

--- Dashboard.php
+++ Dashboard.php
@@ -10,12 +10,14 @@
 </head>
 <body>
    <nav>
-    <li><a href="listOfTrainee.php">List Of Trainee</a></li>
-    <li><a href="listOfMarks.php">Marks Of Student</a></li>
-    <li><a href="addTrainee.php">Add Trainee</a></li>
-    <li><a href="addMarks.php">Add Marks To Trainee</a></li>
+       <li><a href="listOfTrainee.php">List Of Trainee</a></li>
+       <li><a href="listOfMarks.php">Marks Of Student</a></li>
+       <li><a href="addTrainee.php">Add Trainee</a></li>
+       <li><a href="addMarks.php">Add Marks To Trainee</a></li>
    </nav> 
+
   <h2>Welcome <?php echo $_SESSION['Usename'];?></h2> <br>
    
+  <a href="logout.php">Logout</a>
 </body>
 </html>
