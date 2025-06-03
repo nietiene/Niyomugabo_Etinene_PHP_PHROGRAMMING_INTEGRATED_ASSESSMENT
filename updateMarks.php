493d7ba44b4e81fe410fe05066eca25682bc737a
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 19:12:22 -0700

    commit of gikonko-tss-managment-system

--- updateMarks.php
+++ updateMarks.php
@@ -2,9 +2,9 @@
 
    include('conn.php');
    
-   if (isset($_GET['Marks_Id'])) {
-       $Marks_Id = $_GET['Mark_id'];
-       $sql = "SELECT * FROM marks WHERE Mark_Id = '$Marks_Id'";
+   if (isset($_GET['Mark_Id'])) {
+       $Mark_Id = $_GET['Mark_Id'];
+       $sql = "SELECT * FROM marks WHERE Mark_Id = '$Mark_Id'";
        $result = mysqli_query($conn, $sql);
      
        if (mysqli_num_rows($result) > 0 ) {
@@ -25,7 +25,17 @@
 <body>
     <form action="" method="post">
         <label for="">Mark Code</label>
-        <input type="text" name="Mark_Id" value=<?php echo $data['Mark_Id'];?>>
+        <input type="text" name="Mark_Id" value=<?php echo $data['Mark_Id'];?>> <br>
+        <label for="">Trainee Code</label>
+        <input type="text" name="Trainee_Id" value=<?php echo $data['Trainee_Id'];?>> <br>
+        <label for="">Trainee Name</label>
+        <input type="text" name="Trainee_Id" value=<?php echo $data['Trainee_Id'];?>> <br>
+        <label for="">Module Code</label>
+        <input type="text" name="Module_Id" value=<?php echo $data['Module_Id'];?>> <br>
+        <label for="">Formative Assessment</label>
+        <input type="text" name="Formative_Assessment" value=<?php echo $data['Formative_Assessment'];?>> <br>
+        <label for="">Summative Assessment</label>
+        <input type="text" name="Summative_Assessment" value=<?php echo $data['Summative_Assessment'];?>> <br>
     </form>
 </body>
 </html>
