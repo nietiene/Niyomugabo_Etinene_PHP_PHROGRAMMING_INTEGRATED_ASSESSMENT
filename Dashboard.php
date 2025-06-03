b0abf2a64a5a70cd2a08cf4ef6b80632fa68b8ba
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 20:20:58 -0700

    commit of gikonko-tss-managment-system

--- Dashboard.php
+++ Dashboard.php
@@ -10,20 +10,23 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-green-100 min-h-screen">
-    <div class="bg-blue-500 shadow-md">
+    <div class="bg-blue-500 shadow-lg">
       <nav class="container mx-auto flex justify-between items-center py-4 px-6">
         <ul class="flex space-x-6 text-white font-semibold">
-           <li><a href="listOfTrainee.php" class="hover:text-blue-700">List Of Trainee</a></li>
-           <li><a href="listOfTrades.php" class="hover:text-blue-700">List Of Trades</a></li>
-           <li><a href="listOfMarks.php" class="hover:text-blue-700">Marks Of Student</a></li>
-           <li><a href="addTrainee.php" class="hover:text-blue-700">Add Trainee</a></li>
-           <li><a href="addMarks.php" class="hover:text-blue-700">Add Marks To Trainee</a></li>
+           <li><a href="listOfTrainee.php" class="hover:text-blue-800 hover:underline transition duration-200">List Of Trainee</a></li>
+           <li><a href="listOfTrades.php" class="hover:text-blue-800 hover:underline transition duration-200">List Of Trades</a></li>
+           <li><a href="listOfMarks.php" class="hover:text-blue-800 hover:underline transition duration-200">Marks Of Student</a></li>
+           <li><a href="addTrainee.php" class="hover:text-blue-800 hover:underline transition duration-200">Add Trainee</a></li>
+           <li><a href="addMarks.php" class="hover:text-blue-800 hover:underline transition duration-200">Add Marks To Trainee</a></li>
+           <div>
+               <a href="logout.php">Logout</a>
+           </div>   
+           
          </ul>
       </nav> 
    </div>
 
   <h2>Welcome <?php echo $_SESSION['Usename'];?></h2> <br>
-   
-  <a href="logout.php">Logout</a>
+
 </body>
 </html>
