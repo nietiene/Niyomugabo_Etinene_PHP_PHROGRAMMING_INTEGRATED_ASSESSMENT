2d9a6c342aded22649d958cc467478b773ed5980
nietiene <niyomugaboetiene53@gmail.com>
2025-06-02 19:46:24 -0700

    commit of gikonko-tss-managment-system

---
+++ deleteMarks.php
@@ -0,0 +1,13 @@
+<?php
+    include ('conn.php');
+
+     $Mark_Id = $_GET['Mark_Id'];
+     $sql = "DELETE FROM marks WHERE Mark_Id = '$Mark_Id'";
+     $query = mysqli_query($conn, $sql);
+    if ($query) {
+        header("Location:listOfMarks.php");
+    } else {
+        die("ERROR:" . mysqli_error($conn));
+    }
+
+?>
