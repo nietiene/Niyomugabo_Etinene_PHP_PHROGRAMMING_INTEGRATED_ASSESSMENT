246c8ce44a02a67a995b2243688587c341e891fb
nietiene <niyomugaboetiene53@gmail.com>
2025-06-03 19:31:28 -0700

    commit of gikonko-tss-managment-system

--- updateTrade.php
+++ updateTrade.php
@@ -12,6 +12,24 @@
             die("ERROR:" . mysqli_error($conn));
           }
     }
+
+          
+   if (isset($_POST['save'])) {
+         if (!empty($_POST['Trade_name'])) {
+             $Trade_Id = $_POST['Trade_id'];
+             $Trade_Name = $_POST['Trade_name'];
+      
+              $sql = "UPDATE trades SET Trade_name = '$Trade_Name' WHERE Trade_id = '$Trade_Id'";
+              $query = mysqli_query($conn, $sql);
+
+              if ($query) {
+                   header("Location:listOfTrades.php");
+              } else {
+                  die ("ERROR:". mysqli_error($conn));
+              }
+
+           }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -20,34 +38,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Trade</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <form action="" method="post">
-        <label for="">Trade Code</label>
-        <input type="text" name="Trade_id" value=<?php echo $Data['Trade_id']?> readonly> <br>
+<body class="min-h-screen bg-green-100 flex justify-center items-center">
+    <form method="post" class="bg-blue-700 w-full rounded-lg max-w-sm px-4 shadow-2xl">
+      <h1 class="text-2xl text-green-500 font-semibold mb-6 text-center underline">Update Trade</h1>
+        <label class="block text-green-500 font-semibold text-lg">Trade Code</label>
+        <input type="text" name="Trade_id" value=<?php echo $Data['Trade_id']?> readonly
+        class="py-2 w-full focus:ring-2 focus:outline-blue-200 focus:ring-blue-300 bg-blue-200 rounded-lg text-blue-500 mb-6"> <br>
 
-        <label for="">Trade Name</label>
-        <input type="text" name="Trade_name" value=<?php echo $Data['Trade_name']?>> <br>
-      
-        <button name="save">Save Changes</button>
+        <label class="block text-green-500 font-semibold text-lg">Trade Name</label>
+        <input type="text" name="Trade_name" value=<?php echo $Data['Trade_name']?>
+        class="py-2 w-full focus:ring-2 focus:outline-blue-200 focus:ring-blue-300 bg-blue-200 rounded-lg text-blue-500 mb-6"> <br>
+ 
+        <div class="flex justify-between">
+              <button name="save" class="bg-green-500 px-5 py-2 mb-4 rounded-lg shadow-md text-white hover:bg-green-600">Add New</button>
+             <a href="Dashboard.php" class="bg-red-500 px-5 py-2 mb-4 rounded-lg shadow-md text-white hover:bg-red-600">Back</a>
+         </div>
     </form>
 
     <?php
-      
-      if (isset($_POST['save'])) {
-        $Trade_Id = $_POST['Trade_id'];
-        $Trade_Name = $_POST['Trade_name'];
-      
-        $sql = "UPDATE trades SET Trade_name = '$Trade_Name' WHERE Trade_id = '$Trade_Id'";
-        $query = mysqli_query($conn, $sql);
-
-        if ($query) {
-            header("Location:listOfTrades.php");
-        } else {
-            die ("ERROR:". mysqli_error($conn));
-        }
-
-      }
 
 
 
