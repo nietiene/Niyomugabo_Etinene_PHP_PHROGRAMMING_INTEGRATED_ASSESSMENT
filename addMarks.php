5a413a2be3ad507f9810f27c06154b64bc925132
nietiene <niyomugaboetiene53@gmail.com>
2025-06-03 17:05:20 -0700

    commit of gikonko-tss-managment-system

--- addMarks.php
+++ addMarks.php
@@ -1,3 +1,30 @@
+
+  <?php
+
+      $error = "";
+      if (isset($_POST['addMarks'])) {
+        
+        $trainee_code = $_POST['Trainee_Id'];
+        $module_code = $_POST['Module_Id'];
+        $Formative = $_POST['Formative_Assessment'];
+        $Summative = $_POST['Summative_Assessment'];
+       //echo "Trainee Code" . $trainee_code . " <br>module_code" . $module_code . "<br>Fromative" . $Formative . "<br>Summative".$Summative;
+        $Total = $Formative + $Summative;
+        $Decision = ($Total >= 70) ? "Competent" : "Not Competent";
+
+        $sql = "INSERT INTO marks(Trainee_Id, Module_Id, Formative_Assessment, Summative_Assessment, Total_Marks, decision) VALUES('$trainee_code', '$module_code', '$Formative', '$Summative', '$Total', '$Decision    ')";
+        $query = mysqli_query($conn, $sql);
+        
+        if ($query) {
+            echo "Data Inseted Successfully <br><strong>$Decision</strong>";
+        } else {
+            die("ERROR:". mysqli_error($conn));
+        }
+      }
+
+
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,31 +63,13 @@
              <button name="addMarks" class="bg-blue-500 py-2 px-5 text-white rounded-lg shadow-xl hover:bg-blue-600 transition duration-200">Save Marks</button>
              <a href="Dashboard.php" class="bg-red-500 py-2 px-5 text-white rounded-lg shadow-xl hover:bg-red-600 transition duration-200">Back</a>
         </div>
+          <?php if (!empty($error)): ?>
+                <div class="bg-red-100 py-1 text-red-500 rounded border border-red-500 px-1">
+                    <?php echo $error; ?>
+                </div>
+             <?php endif; ?>   
     </form>
 
-    <?php
-
-      if (isset($_POST['addMarks'])) {
-        
-        $trainee_code = $_POST['Trainee_Id'];
-        $module_code = $_POST['Module_Id'];
-        $Formative = $_POST['Formative_Assessment'];
-        $Summative = $_POST['Summative_Assessment'];
-       //echo "Trainee Code" . $trainee_code . " <br>module_code" . $module_code . "<br>Fromative" . $Formative . "<br>Summative".$Summative;
-        $Total = $Formative + $Summative;
-        $Decision = ($Total >= 70) ? "Competent" : "Not Competent";
-
-        $sql = "INSERT INTO marks(Trainee_Id, Module_Id, Formative_Assessment, Summative_Assessment, Total_Marks, decision) VALUES('$trainee_code', '$module_code', '$Formative', '$Summative', '$Total', '$Decision    ')";
-        $query = mysqli_query($conn, $sql);
-        
-        if ($query) {
-            echo "Data Inseted Successfully <br><strong>$Decision</strong>";
-        } else {
-            die("ERROR:". mysqli_error($conn));
-        }
-      }
-
-
-    ?>
+  
 </body>
 </html>
