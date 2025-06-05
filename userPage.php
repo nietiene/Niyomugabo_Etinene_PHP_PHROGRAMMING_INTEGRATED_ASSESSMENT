0fa4a7865429517a21d87f115b64b791bb95bce6
nietiene <niyomugaboetiene53@gmail.com>
2025-06-05 16:00:08 -0700

    commit of gikonko-tss-managment-system

--- userPage.php
+++ userPage.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Page</title>
+    <link href="output.css" rel="stylesheet" />
 </head>
 <body>
     <header>
