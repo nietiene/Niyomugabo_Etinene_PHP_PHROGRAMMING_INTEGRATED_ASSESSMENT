1d5bb575ac8fd5483168989fc079d280a700da80
nietiene <niyomugaboetiene53@gmail.com>
2025-06-05 15:56:05 -0700

    commit of gikonko-tss-managment-system

--- greetingUser.php
+++ greetingUser.php
@@ -2,7 +2,7 @@
    include("conn.php");
    session_start();
 
-   if (!isset($_SESSION['Firstname'])) {
+   if (!isset($_SESSION['Usename'])) {
        $_SESSION['login_error'] = "Please login to access this page";
        header("Location: loginStudent.php");
        exit();
@@ -20,13 +20,13 @@
 <body class="min-h-screen bg-green-100 flex flex-col">
 
   <header class="w-full fixed top-0 left-0 z-50 shadow-md bg-white h-16 flex items-center px-4">
-    <?php include("userDashboard.php"); ?>
+    <?php include("Dashboard.php"); ?>
   </header>
 
   <main class="flex-grow pt-24 flex justify-center items-center px-4">
     <form action="" method="post" class="bg-white p-8 rounded-xl shadow-lg w-full max-w-xl text-center">
       <h2 class="font-semibold text-2xl text-green-600">
-        Welcome <strong><u><?php echo $_SESSION['Firstname']; ?></u></strong> to GIKONKO TSS Trainee Management System
+        Welcome <strong><u><?php echo $_SESSION['Usename']; ?></u></strong> to GIKONKO TSS Trainee Management System
       </h2>
     </form>
   </main>
