b700b26da87503acbc909c3c3770cf34a92b6961
nietiene <niyomugaboetiene53@gmail.com>
2025-06-06 10:59:56 -0700

    commit of gikonko-tss-managment-system

--- download_not_competent.php
+++ download_not_competent.php
@@ -12,20 +12,22 @@ header('Content-Disposition: attachment; filename="competent_trainee.csv"');
 
 $output = fopen("php://output", "w");
 
-fputcsv($output, ['Trainee Code', 'Trainee Name', 'Module Code', 'Module Name',, 'Trade_name', 'Decision']);
+fputcsv($output, ['Trainee Code', 'Trainee Name', 'Module Code', 'Module Name','Trade_name', 'Decision']);
+
 
 $sql = "SELECT m.Trainee_Id, 
         CONCAT(t.Firstname, ' ', t.lastname) AS Trainee_Name,
-        m.Module_Id,
+        m.Module_id,
         md.Module_Name,
-        m.decision,
+        m.Decision,
         m.Total_Marks,
         td.Trade_name
         FROM marks m 
         JOIN Trainees t ON t.Trainee_Id = m.Trainee_Id
-        JOIN modules md ON m.Module_Id = md.Module_Id
-        JOIN trades td ON td.Trade_id = t.Trade_id
+        JOIN modules md ON m.Module_id = md.Module_Id
+        JOIN trades td ON td.Trade_id = t.Trade_Id
         HAVING m.Total_Marks < 70";
+
 
 $query = mysqli_query($conn, $sql);
 
@@ -33,10 +35,10 @@ while ($data = mysqli_fetch_assoc($query)) {
     fputcsv($output, [
         $data['Trainee_Id'],
         $data['Trainee_Name'],
-        $data['Module_Id'],
+        $data['Module_id'],
         $data['Module_Name'],
         $data['Trade_name'],
-        $data['decision']
+        $data['Decision']
     ]);
 }
 
